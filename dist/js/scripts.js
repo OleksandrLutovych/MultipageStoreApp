dd980af43a72a18af5370c1d710a92d670de22ad
Lutovych Oleksandr <seethelight1997@gmail.com>
2022-09-13 19:59:35 +0200

    prettier + carousel code + animation + menu

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,7 +1,6 @@
+const button = document.getElementsByClassName("responsive-btn")[0];
+const navMenu = document.getElementsByClassName("header__nav-menu")[0];
 
-const button = document.getElementsByClassName('responsive-btn') [0]
-const navMenu = document.getElementsByClassName('header__nav-menu')[0]
-
-button.addEventListener('click', () => {
-    navMenu.classList.toggle('active')
-})
+button.addEventListener("click", () => {
+  navMenu.classList.toggle("active");
+});
--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,38 @@
-const {src, dest, watch, series} = require('gulp')
-const sass = require('gulp-sass')(require('sass'))
-const sync = require('browser-sync').create()
-const csso = require('csso')
-const concat = require('gulp-concat')
-const include = require('gulp-file-include')
+const { src, dest, watch, series } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const sync = require("browser-sync").create();
+const csso = require("csso");
+const concat = require("gulp-concat");
+const include = require("gulp-file-include");
 
-function html () {
-    return src('src/html/**.html')
+function html() {
+  return src("src/html/**.html");
 }
 
-function scss () {
-    return src('src/scss/**.scss')
+function scss() {
+  return src("src/scss/**.scss")
     .pipe(sass())
-    .pipe(concat('styles.css'))
-    .pipe(dest('dist/css'))
+    .pipe(concat("styles.css"))
+    .pipe(dest("dist/css"));
 }
 
 function serve() {
-    sync.init({
-        server: '/dist'
-    })
-    watch('src/html/**.html', series(html)).on('change', sync.reload)
-    watch('src/scss/**.scss', series(scss)).on('change', sync.reload)
+  sync.init({
+    server: "/dist",
+  });
+  watch("src/html/**.html", series(html)).on("change", sync.reload);
+  watch("src/scss/**.scss", series(scss)).on("change", sync.reload);
 }
 
-function server () {
+function server() {
+  sync.init({
+    server: "./src/html",
+  });
 
-    sync.init({
-        server: "./src/html"
-    });
-
-    watch("src/scss/**.scss");
-    watch("src/*.html").on('change', sync.reload);
+  watch("src/scss/**.scss");
+  watch("src/*.html").on("change", sync.reload);
 }
 
-exports.server = server
-exports.serve = series(scss)
-exports.scss = scss 
+exports.server = server;
+exports.serve = series(scss);
+exports.scss = scss;
