cd2198dc2e5247e966454b58f46d9203e923fdf3
Lutovych Oleksandr <seethelight1997@gmail.com>
2022-09-18 14:03:09 +0200

    var + responsive btn css

--- dist/js/scripts.js
+++
@@ -1,6 +0,0 @@
-const button = document.getElementsByClassName("responsive-btn")[0];
-const navMenu = document.getElementsByClassName("header__nav-menu")[0];
-
-button.addEventListener("click", () => {
-  navMenu.classList.toggle("active");
-});
