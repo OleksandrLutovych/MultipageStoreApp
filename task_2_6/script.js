42ae15d0170e6858f31cd59aef893f7c00d3c53e
Lutovych Oleksandr <seethelight1997@gmail.com>
2022-11-03 18:31:45 +0100

    refactoring code

--- task_2_6/script.js
+++ task_2_6/script.js
@@ -1,18 +1,17 @@
 const form = document.getElementById("form");
 const submit = document.getElementById("submit");
-const todoContent = document.getElementById("todo-content");
 const clearAll = document.getElementById("clear-all");
 const todoBox = document.querySelector("#todo-container");
 const input = document.getElementById("input");
-let date = new Date(Date.now() + 86400e3).toUTCString();
-let number;
-let cookieArr = document.cookie.split(";");
-console.log(cookieArr.length)
-const toHTML = (item, index) => {
-  return `<div class="todo-element__box">
-    <label class="todo-element__box_content" id="todo-content" >${item}</label>
+let expiresDate = new Date(Date.now() + 86400e3).toUTCString();
+let cookieArr = document.cookie.split("; ");
+
+
+const toHTML = (content, id) => {
+  return `<div class="todo-element__box" id="${id}">
+    <label class="todo-element__box_content" id="todo-content" >${content}</label>
     <input type="checkbox">
-    <a href="" class="clear-todo" data-action="${index}">&#10007;</a>
+    <a href="" class="clear-todo" onclick="event.preventDefault(); deleteItem('${id}')">&#10007;</a>
 </div>`;
 };
 
@@ -21,62 +20,61 @@ function getCookie(name) {
   const parts = value.split(`; ${name}=`);
   if (parts.length === 2) return parts.pop().split(";").shift();
 }
-function setCookie(name, value, options = {}) {
-  options = {
-    path: "/",
-    ...options,
-  };
-  if (options.expires instanceof Date) {
-    options.expires = options.expires.toUTCString();
-  }
-  let updatedCookie = encodeURIComponent(name) + "=" + value;
-  for (let optionKey in options) {
-    updatedCookie += "; " + optionKey;
-    let optionValue = options[optionKey];
-    if (optionValue !== true) {
-      updatedCookie += "=" + optionValue;
-    }
-  }
-  document.cookie = updatedCookie;
+function setCookie(name, value) {
+  document.cookie = `${name}=${value}; expires=${expiresDate}` ;
 }
 function deleteCookie(name) {
-  setCookie(name, "", {
-    "max-age": -1,
-  });
+  document.cookie = name + "=; max-age=-1";
 }
+//- ------------------------ Cookie func
 
 function addTask() {
-  if (document.cookie === "" || document.cookie === null) number = 0;
-  else {
-    number = +cookieArr[cookieArr.length-1].trim().split("")[0];
-  }
-  console.log(number);
-  form.addEventListener("submit", () => {
-    if (input.value) {
-      setCookie(number, input.value);
-      number = number + 1;
-      renderElement();
+  form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    if (input.value && input.value.trim() !== '') {
+      let cookieId = new Date();
+      setCookie("todo" + cookieId.getTime(), input.value);
+      renderElement('todo'+ cookieId.getTime(), input.value)
+      input.value = null
     }
   });
 }
-function renderElement() {
-  if (document.cookie === "" || document.cookie === null) {
-    todoBox.innerHTML = toHTML("add your new task...");
-  } else {
-    todoBox.innerHTML = cookieArr
-      .map((e, index) => {
-        todoBox && todoBox.addEventListener("click", btnHandler);
-        return toHTML(getCookie(`${index}`), index);
-      })
-      .join("");
+function renderElement(id, value) {
+  todoBox.innerHTML += toHTML(getCookie(id, value), id);
+}
+
+function deleteItem(id) {
+  document.getElementById(id).remove()
+  deleteCookie(id);
+}
+
+function check() {
+  cookieArr.sort();
+  let part;
+  for (let i = 0; i < cookieArr.length; i++) {
+    part = cookieArr[i].split("=");
+    if (part[0]) {
+      try {
+        renderElement(part[0],decodeURI(part[1]));
+      } catch (error) {
+        console.log(error.message + " " + part[1]);
+      }
+    }
   }
 }
 const btnHandler = (e) => {
+    e.preventDefault()
     const btn = e.target;
-    console.log(btn.dataset.action);
-    if (btn.tagName === "A") {
-      deleteCookie(`${btn.dataset.action}`);
+    // console.log(btn);
+    if (btn.tagName === "label") {
+        todoContent.style.display = 'none'
     }
-  };
-addTask();
-renderElement();
+    };
+    
+function main() {
+  addTask();
+  check();  
+  todoBox.addEventListener("click", btnHandler);
+}
+
+main();
