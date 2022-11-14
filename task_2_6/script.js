b8ce3dbac342fff6aa02b0bd5172ebb0f3e2faa4
Lutovych Oleksandr <seethelight1997@gmail.com>
2022-11-14 21:22:57 +0100

    refactoring edit method

--- task_2_6/script.js
+++ task_2_6/script.js
@@ -2,16 +2,14 @@ const form = document.forms.form;
 const clearAll = document.getElementById("clear-all");
 const todoBox = document.querySelector("#todo-container");
 const input = document.getElementById("input");
-const editBtn = document.getElementById("edit");
 
-let todoId;
-let isTodoTarget = false;
 let expiresDate = new Date(Date.now() + 86400e3).toUTCString();
 let cookieArr = document.cookie.split("; ");
 
 const toHTML = (content, id) => {
-  return `<div class="todo-element__box" id="${id}">
+  return `<div class="todo-element__box">
     <span class="todo-element__box_content" id="${id}">${content}</span>
+    <button class="edit-todo">Edit</button>
     <button class="clear-todo">&#10007</button>
 </div>`;
 };
@@ -35,7 +33,6 @@ check();
 form.addEventListener("submit", addTodo);
 todoBox.addEventListener("click", todoItemHandle);
 clearAll.addEventListener("click", deleteAll);
-editBtn.addEventListener("click", editTodo);
 
 function addTodo(e) {
   e.preventDefault();
@@ -72,7 +69,6 @@ function todoItemHandle(e) {
   const btn = e.target;
   const item = btn.closest(".todo-element__box");
   let todo;
-
   if (btn.className === "clear-todo" && item) {
     e.preventDefault();
     deleteItem(item);
@@ -80,28 +76,36 @@ function todoItemHandle(e) {
 
   if (btn.tagName === "SPAN" && item) {
     const todoText = item.querySelector("span");
-    input.value = todoText.innerHTML;
-    todoId = todoText.id;
-    console.log(todoId);
-    isTodoTarget = true;
+    const editInput = document.createElement("input");
+    const editBtn = item.querySelector(".edit-todo");
+
+    todoText.replaceWith(editInput);
+    editInput.value = todoText.innerHTML;
+    editBtn.style.display = "block";
+
+    editInput.addEventListener("keyup", (e) => {
+      if (e.keyCode === 13) {
+        editTodo(todoText, editInput, item, editBtn);
+      }
+    });
+    editBtn.addEventListener("click", (e) => {
+      editTodo(todoText, editInput, item, editBtn);
+    });
   }
 }
-editBtn.addEventListener("click", editTodo);
-function editTodo(e) {
-  const todoText = document.getElementById(`${todoId}`);
-  if (input.value.trim() && isTodoTarget) {
-    setCookie(todoId, input.value);
-    todoText.innerHTML = `<span class="todo-element__box_content" id="${todoId}">${input.value}</span>
-    <button class="clear-todo">&#10007</button>`;
-    isTodoTarget = false;
-    input.value = "";
-  } else if (input.value.trim() == "" && isTodoTarget) {
-    
-    deleteCookie(todoId);
-    todoText.remove()
+
+function editTodo(todo, input, item, btn) {
+  if (input.value.trim()) {
+    setCookie(todo.id, input.value);
+    todo.textContent = input.value;
+  } else if (input.value.trim() == "") {
+    deleteCookie(todo.id);
+    item.remove();
   } else {
     return;
   }
+  btn.style.display = "none";
+  input.replaceWith(todo);
 }
 
 function deleteItem(item) {
