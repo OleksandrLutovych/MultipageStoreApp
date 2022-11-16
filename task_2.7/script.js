c1dfacc70797b0336d0028d95778d6d7ad3d11a7
Lutovych Oleksandr <seethelight1997@gmail.com>
2022-11-16 17:46:35 +0100

    task_2.7

--- task_2.7/script.js
+++ task_2.7/script.js
@@ -1,3 +1,20 @@
-fetch('https://restcountries.com/v3.1/all')
-.then(response => response.json())
-.then(item => console.log(item))
+const changeModeBtn = document.getElementById("darkmode");
+const form = document.forms;
+const search = document.querySelector("input");
+const regionSelect = document.getElementById("region-select");
+const contentBox = document.querySelector('.content__wrapper')
+
+
+const renderCountry = (country) => {
+  `<div class="country__card">
+    <img src="https://flagcdn.com/w320/mr.png" alt="">
+    <span class="country">Afganistan</span>
+    <p><span>Population: </span></p>
+    <p><span>Region: </span></p>
+    <p><span>Capital: </span></p>
+</div>`;
+};
+ fetch("https://restcountries.com/v3.1/all")
+    .then((response) => response.json())
+    .then(item => item)
+
