99328c1326a260275b7c4c0eae75e2f0e4caa200
Lutovych Oleksandr <seethelight1997@gmail.com>
2022-11-17 15:40:24 +0100

    add search / add type select

--- task_2.7/script.js
+++ task_2.7/script.js
@@ -1,20 +1,73 @@
 const changeModeBtn = document.getElementById("darkmode");
-const form = document.forms;
+const form = document.querySelector(".main__header");
 const search = document.querySelector("input");
 const regionSelect = document.getElementById("region-select");
-const contentBox = document.querySelector('.content__wrapper')
+const contentBox = document.querySelector(".content__wrapper");
+const API = "https://restcountries.com/v3.1/all";
 
+const apiData = fetch(API).then((response) => response.json());
 
 const renderCountry = (country) => {
-  `<div class="country__card">
-    <img src="https://flagcdn.com/w320/mr.png" alt="">
-    <span class="country">Afganistan</span>
-    <p><span>Population: </span></p>
-    <p><span>Region: </span></p>
-    <p><span>Capital: </span></p>
+  return `<div class="country__card">
+    <img src="${country.flags.png}" alt="">
+    <span class="country">${country.name.common}</span>
+    <p><span>Population: </span>${country.population}</p>
+    <p><span>Region: </span>${country.region}</p>
+    <p><span>Capital: </span>${country.capital}</p>
 </div>`;
 };
- fetch("https://restcountries.com/v3.1/all")
-    .then((response) => response.json())
-    .then(item => item)
 
+function allCountries() {
+  apiData.then((countries) => {
+    countries.map((item) => {
+      if (!search.value.trim()) {
+        contentBox.innerHTML += renderCountry(item);
+      } else {
+        let result = search.value;
+        contentBox.innerHTML += result;
+      }
+    });
+  });
+}
+
+function searchCountries(e) {
+  e.preventDefault();
+  let result = search.value;
+
+  apiData.then((countries) => {
+    contentBox.innerHTML = countries
+      .map((item) => {
+        if (item.name.common.toLowerCase().startsWith(result.trim())) {
+          return renderCountry(item);
+        }
+      })
+      .join("");
+    if (!search.value.trim()) {
+      contentBox.innerHTML = countries.map(renderCountry).join("");
+    }
+  });
+}
+
+function filterCountries(e) {
+  const type = regionSelect.value;
+  apiData.then((countries) => {
+    contentBox.innerHTML = countries
+      .map((item) => {
+        if (item.region === type) {
+          return renderCountry(item);
+        }
+      })
+      .join(" ");
+    if (type === "All") {
+      contentBox.innerHTML = countries.map(renderCountry).join("");
+    }
+  });
+}
+
+allCountries();
+regionSelect.addEventListener("change", filterCountries);
+search.addEventListener("keyup", searchCountries);
+changeModeBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  document.body.classList.toggle("dark-mode");
+});
