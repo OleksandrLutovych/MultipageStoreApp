59f457a8c63691794ed724425b6221a672a079c8
Lutovych Oleksandr <seethelight1997@gmail.com>
2022-12-04 18:49:01 +0100

    company filter counter + disable/enabled button

--- task_2.8/script.js
+++ task_2.8/script.js
@@ -5,7 +5,8 @@ const form = document.querySelector(".users__bar");
 const rangeInput = document.getElementById("price-input");
 const searchBar = document.getElementById("search-bar");
 const shoppingCardBtn = document.getElementById("basket");
-const addToCartBtn = document.querySelectorAll('.add-to-cart')
+const addToCartBtn = document.querySelectorAll(".add-to-cart");
+const companyNameBlock = document.querySelector(".company-name-box");
 
 const shopItems = "http://127.0.0.1:5500/products.json";
 
@@ -14,20 +15,34 @@ function getItems() {
     return response.json();
   });
 }
-const renderElements = (item) => {
-  return `<a href="" class="product__card">
+const renderElements = (item) =>
+  `<a href="" class="product__card">
       <img src="${item.image}" alt="">
       <span class="product__card_name">${item.name}</span>
       <span class="product__card_price">${item.price}</span>
       <button class="add-to-cart">Add to cart</button>
   </a>`;
-};
+
+const renderElementsInShopingCart = (item) =>
+  `<div class="shopping-card__item">
+  <img src="${item.image}" alt="">
+  <div class="shopping-card__item_info">
+      <span>${item.name}</span>
+      <span>${item.price}</span>
+      <button>remove</button>
+  </div>
+  <div class="toggle-arrow">
+      <button>&#8743;</button>
+      <span>4</span>
+      <button>&#8744;</button>
+  </div>
+</div>`;
+
 // ------- Home page
 function mainFeaturedCardsRender() {
-  var productBox = document.querySelector(".cards__wrapper");
+  const productBox = document.querySelector(".cards__wrapper");
   getItems().then((itemsArr) => {
     productBox.innerHTML += itemsArr.map(renderElements).join("");
-
     allProductsBtn.addEventListener("click", () => {
       let card = document.querySelectorAll(".product__card");
 
@@ -41,23 +56,40 @@ function mainFeaturedCardsRender() {
 function productsAllRender() {
   getItems().then((itemsArr) => {
     productsItemBox.innerHTML += itemsArr.map(renderElements).join("");
-
-    companyNameItemsCounter[0].innerHTML = itemsArr.length;
-    companyNameItemsCounter[1].innerHTML = itemsArr.filter(
-      (item) => item.type === "Ikea"
-    ).length;
-    companyNameItemsCounter[2].innerHTML = itemsArr.filter(
-      (item) => item.type === "Marcos"
-    ).length;
-    companyNameItemsCounter[3].innerHTML = itemsArr.filter(
-      (item) => item.type === "Caressa"
-    ).length;
-    companyNameItemsCounter[4].innerHTML = itemsArr.filter(
-      (item) => item.type === "Liddy"
-    ).length;
-
-    console.log(companyNameItemsCounter);
+    countOfElement(itemsArr);
   });
+}
+function countOfElement(arr) {
+  const hashmap = arr.reduce(
+    (accum, { type }, _, array) => {
+      return {
+        ...accum,
+        [type]: array.filter((i) => {
+          if (
+            i.type === type &&
+            i.name.toLowerCase().startsWith(searchBar.value.trim()) &&
+            Math.floor(i.price) < rangeInput.value
+          )
+            return i;
+        }).length,
+      };
+    },
+    { Ikea: 0, Marcos: 0, Caressa: 0, Liddy: 0 }
+  );
+
+  companyNameItemsCounter[0].innerHTML = arr.length;
+  companyNameItemsCounter[1].innerHTML = hashmap.Ikea;
+  companyNameItemsCounter[2].innerHTML = hashmap.Marcos;
+  companyNameItemsCounter[3].innerHTML = hashmap.Caressa;
+  companyNameItemsCounter[4].innerHTML = hashmap.Liddy;
+
+  for (let block of companyNameBlock.children) {
+    if (block.children[1].innerHTML < 1) {
+      block.children[0].disabled = true;
+    } else {
+      block.children[0].disabled = false;
+    }
+  }
 }
 
 function searchBySearchBar() {
@@ -65,70 +97,43 @@ function searchBySearchBar() {
     searchBar.addEventListener("input", () => {
       const result = itemsArr.filter((item) => {
         if (
-          item.name.toLowerCase().startsWith(searchBar.value.trim()) &&
-          Math.floor(item.price) < rangeInput.value || 
-          item.name.toUpperCase().startsWith(searchBar.value.trim())
-        )
+          (item.name.toLowerCase().startsWith(searchBar.value.trim()) ||
+          item.name.toUpperCase().startsWith(searchBar.value.trim())) &&
+            Math.floor(item.price) < rangeInput.value
+        ) 
           return item;
       });
       productsItemBox.innerHTML = result.map(renderElements).join("");
-      companyNameItemsCounter[0].innerHTML = result.length;
-      companyNameItemsCounter[1].innerHTML = itemsArr.filter((item) => {
-        if (
-          item.name.toLowerCase().startsWith(searchBar.value.trim()) &&
-          item.type === "Ikea" &&
-          Math.floor(item.price) < rangeInput.value
-        ) {
-          return item;
-        }
-      }).length;
-      companyNameItemsCounter[2].innerHTML = itemsArr.filter((item) => {
-        if (
-          item.name.toLowerCase().startsWith(searchBar.value.trim()) &&
-          item.type === "Marcos" &&
-          Math.floor(item.price) < rangeInput.value
-        ) {
-          return item;
-        }
-      }).length;
-      companyNameItemsCounter[3].innerHTML = itemsArr.filter((item) => {
-        if (
-          item.name.toLowerCase().startsWith(searchBar.value.trim()) &&
-          item.type === "Caressa" &&
-          Math.floor(item.price) < rangeInput.value
-        ) {
-          return item;
-        }
-      }).length;
-      companyNameItemsCounter[4].innerHTML = itemsArr.filter((item) => {
-        if (
-          item.name.toLowerCase().startsWith(searchBar.value.trim()) &&
-          item.type === "Liddy" &&
-          Math.floor(item.price) < rangeInput.value
-        ) {
-          return item;
-        }
-      }).length;
+      countOfElement(result);
     });
   });
 }
 
 function sortedByType() {
-  const companyNameBlock = document.querySelector(".company-name-box");
-
   getItems().then((itemsArr) => {
     companyNameBlock.addEventListener("click", (e) => {
       e.preventDefault();
       const btn = e.target;
-      if (btn.innerHTML === "All") {
+
+      if (btn.tagName !== "BUTTON") return;
+      if (btn.innerHTML === "All")
         return (productsItemBox.innerHTML = itemsArr
-          .map(renderElements)
+          .map((item) => {
+            if (
+              item.name.toLowerCase().startsWith(searchBar.value.trim()) &&
+              Math.floor(item.price) < rangeInput.value
+            )
+              return renderElements(item);
+          })
           .join(""));
-      }
       productsItemBox.innerHTML = itemsArr
         .map((item) => {
           if (btn.innerHTML === item.type) {
-            return renderElements(item);
+            if (
+              item.name.toLowerCase().startsWith(searchBar.value.trim()) &&
+              Math.floor(item.price) < rangeInput.value
+            )
+              return renderElements(item);
           }
         })
         .join("");
@@ -148,73 +153,55 @@ function searchItemByRange() {
       const result = itemsArr.filter((item) => {
         if (
           Math.floor(item.price) < rangeInput.value &&
-          item.name.toLowerCase().startsWith(searchBar.value.trim()) 
-        ) {
+          item.name.toLowerCase().startsWith(searchBar.value.trim())
+        )
           return item;
-        }
       });
       productsItemBox.innerHTML = result.map(renderElements).join("");
       // -- Counter of element
-      companyNameItemsCounter[0].innerHTML = result.length;
+      countOfElement(result);
     });
   });
 }
 
-productsItemBox.addEventListener('click', (e)=> {
-  e.preventDefault()
-  if(e.target.className === "add-to-cart" ) {
-    const result = e.target.parentElement
-    const key = result.querySelector('.product__card_name').innerHTML
-    const value = result.querySelector('.product__card_price').innerHTML
-    console.log(key, value)
-    localStorage.setItem(key, value)
-  }
-})
+// ------- Shopping cart
+function addItemtoShoppingCard() {
+  const shoppingCardWrapper = document.querySelector(
+    ".shopping-card__item-wrapper"
+  );
+
+  getItems().then((itemsArr) => {
+    const storageArr = itemsArr.filter((item) => {
+      for (let key in localStorage) {
+        if (!localStorage.hasOwnProperty(key)) continue;
+        if (key === item.name) return item;
+      }
+    });
+    shoppingCardWrapper.innerHTML += storageArr
+      .map(renderElementsInShopingCart)
+      .join("");
+  });
+}
 shoppingCardBtn.addEventListener("click", (e) => {
-  e.preventDefault()
-  const shoppingCard = document.getElementById('shopping-card')
-  shoppingCard.classList.toggle('show')
+  e.preventDefault();
+  const shoppingCard = document.getElementById("shopping-card");
+  shoppingCard.classList.toggle("show");
 });
 
-for (let key in localStorage) {
-  if (!localStorage.hasOwnProperty(key)) {
-    continue;
-  }
-  console.log(key)
-}
 mainFeaturedCardsRender();
 productsAllRender();
+addItemtoShoppingCard();
 searchBySearchBar();
 sortedByType();
 searchItemByRange();
 
-
-
-// searchItemAllFilterTest();
-
-// function searchItemAllFilterTest() {
-//   const rangeInput = document.getElementById("price");
-//   const rangeInputValueInfo = document.querySelector("output");
-//   rangeInputValueInfo.innerHTML = rangeInput.value;
-
-//   getItems().then((itemsArr) => {
-//     form.addEventListener("input", (e) => {
-//       e.preventDefault();
-//       const target = e.target;
-
-//       if (target.id === "price") {
-//         console.log(target);
-//         productsItemBox.innerHTML = itemsArr
-//           .map((item) => {
-//             if (Math.floor(item.price) < rangeInput.value) {
-//               return renderElements(item);
-//             }
-//           })
-//           .join("");
-//       }
-//       if (target.id === "search-bar") {
-//         console.log("test");
-//       }
-//     });
-//   });
-// }
+productsItemBox.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (e.target.className === "add-to-cart") {
+    const result = e.target.parentElement;
+    const key = result.querySelector(".product__card_name").innerHTML;
+    const value = result.querySelector(".product__card_price").innerHTML;
+    console.log(key, value);
+    localStorage.setItem(key, value);
+  }
+});
